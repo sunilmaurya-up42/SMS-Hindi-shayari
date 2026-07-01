6a8bb08ae35807333dc415e471b119e0cf5c9a70
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-07-01 17:31:19 +0530

    Update connectDatabase.js

--- backend/src/database/connectDatabase.js
+++ backend/src/database/connectDatabase.js
@@ -1,20 +1,32 @@
 import mongoose from "mongoose";
+import logger from "../config/logger.js";
 
 export const connectDatabase = async (mongoUri) => {
   try {
     mongoose.set("strictQuery", true);
 
-    await mongoose.connect(mongoUri);
+    const connection = await mongoose.connect(mongoUri, {
+      dbName: "sms_hindi_shayari"
+    });
 
-    console.log("=================================");
-    console.log("✅ MongoDB Connected Successfully");
-    console.log("=================================");
+    logger.info(
+      `MongoDB Connected : ${connection.connection.host}`
+    );
+
+    mongoose.connection.on("disconnected", () => {
+      logger.warn("MongoDB Disconnected");
+    });
+
+    mongoose.connection.on("reconnected", () => {
+      logger.info("MongoDB Reconnected");
+    });
+
+    mongoose.connection.on("error", (error) => {
+      logger.error(error.message);
+    });
+
   } catch (error) {
-    console.error("=================================");
-    console.error("❌ MongoDB Connection Failed");
-    console.error(error.message);
-    console.error("=================================");
-
+    logger.error(error.message);
     process.exit(1);
   }
 };
