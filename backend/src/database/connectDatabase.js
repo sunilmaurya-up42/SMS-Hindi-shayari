ad5cea216d6657149c5751a1b429998211fcb5aa
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-07-01 17:00:20 +0530

    Update connectDatabase.js

--- backend/src/database/connectDatabase.js
+++ backend/src/database/connectDatabase.js
@@ -2,12 +2,18 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async (mongoUri) => {
   try {
+    mongoose.set("strictQuery", true);
+
     await mongoose.connect(mongoUri);
 
-    console.log("✅ MongoDB Connected");
+    console.log("=================================");
+    console.log("✅ MongoDB Connected Successfully");
+    console.log("=================================");
   } catch (error) {
+    console.error("=================================");
     console.error("❌ MongoDB Connection Failed");
     console.error(error.message);
+    console.error("=================================");
 
     process.exit(1);
   }
