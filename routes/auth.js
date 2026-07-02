e8728ea25d70515be43dc95d248d9f7553452792
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-07-02 23:44:46 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {
     isGuest,
     isAuthenticated
