8aa38aef963a4c07d1f6874ae403845155b4303c
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-08-09 02:18:49 +0530

    config

---
+++ passport.js
@@ -0,0 +1,75 @@
+/**
+ * ==========================================================
+ * SMS Hindi Shayari
+ * Passport Configuration
+ * ==========================================================
+ */
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Admin = require("../models/Admin");
+const bcrypt = require("bcryptjs");
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "email"
+        },
+        async (email, password, done) => {
+
+            try {
+
+                const admin = await Admin.findOne({
+                    email: email.toLowerCase(),
+                    }).select("+password");
+
+                if (!admin) {
+                    return done(null, false, {
+                        message: "Invalid email or password."
+                    });
+                }
+
+                const isMatch = await bcrypt.compare(
+                    password,
+                    admin.password
+                );
+
+                if (!isMatch) {
+                    return done(null, false, {
+                        message: "Invalid email or password."
+                    });
+                }
+
+                return done(null, admin);
+
+            } catch (err) {
+
+                return done(err);
+
+            }
+
+        }
+    )
+);
+
+passport.serializeUser((admin, done) => {
+    done(null, admin.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+
+    try {
+
+        const admin = await Admin.findById(id).select("-password");
+
+        done(null, admin);
+
+    } catch (err) {
+
+        done(err);
+
+    }
+
+});
+
+module.exports = passport;
