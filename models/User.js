b59eaa83a0f79baf05c9288b67ebf88af4a2bc71
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-08-09 00:03:09 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,54 +2,63 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-{
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 100
+        },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
 
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        select: false
-    },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+            select: false
+        },
 
-    avatar: {
-        type: String,
-        default: ""
-    },
+        avatar: {
+            type: String,
+            default: ""
+        },
 
-    role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
-    },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
 
-    isActive: {
-        type: Boolean,
-        default: true
-    },
+        isActive: {
+            type: Boolean,
+            default: true,
+            index: true
+        },
 
-    lastLogin: {
-        type: Date,
-        default: null
+        lastLogin: {
+            type: Date,
+            default: null
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
     }
-},
-{
-    timestamps: true,
-    versionKey: false
-}
 );
+
+/*
+|--------------------------------------------------------------------------
+| Hash Password
+|--------------------------------------------------------------------------
+*/
 
 userSchema.pre("save", async function (next) {
 
@@ -57,15 +66,38 @@ userSchema.pre("save", async function (next) {
         return next();
     }
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    try {
 
-    next();
+        const salt = await bcrypt.genSalt(10);
+
+        this.password =
+            await bcrypt.hash(this.password, salt);
+
+        next();
+
+    } catch (error) {
+
+        next(error);
+
+    }
 
 });
 
-userSchema.methods.comparePassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-};
+/*
+|--------------------------------------------------------------------------
+| Compare Password
+|--------------------------------------------------------------------------
+*/
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.methods.comparePassword =
+    async function (password) {
+
+        return bcrypt.compare(
+            password,
+            this.password
+        );
+
+    };
+
+module.exports =
+    mongoose.model("User", userSchema);
